fc18ac8c8990590ba3229e1519d9a5240036bab2
Niklas Bremen <niklas@gbvnt.de>
2018-11-18 22:39:50 +0100

    Integration Footer mit Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import NavigationBar from "./components/navigation/NavigationBar/NavigationBar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavigationBar />
+        <Footer />
       </div>
     );
   }
