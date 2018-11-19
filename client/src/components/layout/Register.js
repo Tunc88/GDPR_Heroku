27b93df579c49d1391ca2a5a6ca5eaddbd02f86e
monikasemwal <monika.semwal@tum.de>
2018-11-19 13:44:39 +0100

    register

--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Panel } from "react-bootstrap";
+
+import "./Register.css";
 
 export default class Register extends Component {
   render() {
     return (
       <div>
-        <h1>Register</h1>
+        <Panel>
+          <Panel.Body />
+        </Panel>
       </div>
     );
   }
