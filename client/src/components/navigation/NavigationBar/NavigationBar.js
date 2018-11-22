8debe3f6d337f37ef5fb0294354d63d6d8614c3d
Tuncay <tuncay.tugrul@tum.de>
2018-11-22 15:59:28 +0100

    inserted logged in and profile section

--- client/src/components/navigation/NavigationBar/NavigationBar.js
+++ client/src/components/navigation/NavigationBar/NavigationBar.js
@@ -15,7 +15,26 @@ import { Link } from "react-router-dom";
 
 import classes from "./NavigationBar.css";
 
+var isLoggedIn = false;
+
 class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("The search was submitted " + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <Navbar inverse collapseOnSelect>
@@ -33,10 +52,14 @@ class NavigationBar extends React.Component {
                 id="search"
                 type="text"
                 placeholder="Enter concern or pattern"
+                value={this.state.value}
+                onChange={this.handleChange}
               />
             </FormGroup>{" "}
             <Link to="/search">
-              <Button type="submit">Search!</Button>
+              <Button onClick={this.handleChange} value="Submit" type="submit">
+                Search!
+              </Button>
             </Link>{" "}
             <Link to="/overview">
               <Button type="submit">Back to overview</Button>
@@ -46,22 +69,39 @@ class NavigationBar extends React.Component {
             <div className="placeholderRight" />
             <FormGroup>
               <img
+                alt="userPic"
                 id="userPic"
                 src="https://venturebeat.com/wp-content/uploads/2016/02/anonymous-face.shutterstock_365080829.jpg?fit=400%2C320&strip=all"
               />
-              <Nav>
-                <NavDropdown
-                  className="profile"
-                  eventKey={3}
-                  title="Profile"
-                  id="basic-nav-dropdown"
-                >
-                  <MenuItem eventKey={3.1}>View</MenuItem>
-                  <MenuItem eventKey={3.2}>Edit</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.3}>Change Role</MenuItem>
-                </NavDropdown>
-              </Nav>
+
+              {isLoggedIn == false ? (
+                <Nav>
+                  <NavItem eventKey={1} href="/login">
+                    Login
+                  </NavItem>
+                  <NavItem eventKey={2} href="/register">
+                    Register
+                  </NavItem>
+                  <Navbar.Text className="roleText">Guest</Navbar.Text>
+                </Nav>
+              ) : (
+                <Nav>
+                  <NavDropdown
+                    className="profile"
+                    eventKey={3}
+                    title="Profile"
+                    id="basic-nav-dropdown"
+                  >
+                    <MenuItem eventKey={3.1}>View</MenuItem>
+                    <MenuItem eventKey={3.2}>Edit</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem eventKey={3.3}>Change Role</MenuItem>
+                  </NavDropdown>
+                  <Navbar.Text className="roleText">
+                    Data Protection Officer
+                  </Navbar.Text>
+                </Nav>
+              )}
             </FormGroup>
           </Navbar.Form>
         </Navbar.Collapse>
